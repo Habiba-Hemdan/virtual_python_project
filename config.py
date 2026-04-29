4844312709a2d776fa8fee7d6bb7423aba83fa5f
Haneen <haneen8924@gmail.com>
2026-04-29 18:36:53 +0300

    Restoring all project files

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
  # Placeholder for future logic
+COLORS = {
+    "primary": "#FCC900",
+    "bg_light": "#F8F9FA",
+    "white": "#FFFFFF",
+    "text_dark": "#2D2D2D",
+    "text_muted": "#777777",
+    "accent": "#000000"
+}
--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,5 @@
-
+from .restaurant_page import RestaurantPage
+from .menu_page import MenuPage
+from .item_details_page import ItemDetailsPage
+from .cart_page import CartPage
+from .checkout_page import CheckoutPage
